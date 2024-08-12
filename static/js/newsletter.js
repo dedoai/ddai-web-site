21d9c80dea552bd4105dbae42781e845bfc58fe9
Magidan <daniele.magiera@dedoai.org>
2024-08-12 12:28:55 +0000

    dp-96-b-wip

--- static/js/newsletter.js
+++ static/js/newsletter.js
@@ -1,27 +1,39 @@
 document.addEventListener('DOMContentLoaded', function () {
+    var overlay = document.getElementById('form-overlay');
     document.querySelector('#nlfrm').addEventListener('submit', function (event) {
       event.preventDefault();
-      var data = this;
-      let formData = new FormData(data);
+      overlay.classList.toggle('active');
+      var frm = this;
+      let formData = new FormData(frm);
       const dataObject = Object.fromEntries(formData.entries());
       const j_body = JSON.stringify(dataObject);
-      console.log('j_body', j_body);
-      fetch(data.getAttribute('action'), {
-        method: data.getAttribute('method'),
+      
+      document.querySelector('.nl-deny').addEventListener('click', function (event) {
+         overlay.classList.toggle('active');
+         return;
+      });
+
+      document.querySelector('.nl-accept').addEventListener('click', function(event){
+        event.preventDefault();
+        fetch(frm.getAttribute('action'), {
+        method: frm.getAttribute('method'),
         body: j_body
       })
-        .then((response) => {
-          if (response.status === 200) {
-            return response.json();
-          } else {
-            throw new Error("Something went wrong on server!");
-          }
-        })
-        .then((response) => {
-          // sconsole.log( response );
-          let resp = JSON.parse(response.body);
-          let respbox = document.querySelector('#nlrsp');
-          respbox.innerHTML = '<span>' + resp.message + '</span>';
-        })
+      .then((response) => {
+         if (response.status === 200) {
+         return response.json();
+         } else {
+         const err = "Something went wrong on server!";
+         respbox.innerHTML = '<span>' + err + '</span>';
+         throw new Error(err);
+         }
+      })
+      .then((response) => {
+         let resp = JSON.parse(response.body);
+         let respbox = document.querySelector('#nlrsp');
+         respbox.innerHTML = '<span>' + resp.message + '</span>';
+         setTimeout( overlay.classList.toggle('active'), 1000 );
+      });
     });
   });
+});
