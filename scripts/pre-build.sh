6415dbbaf7c4feed02cee27f5a9fd4ae1e561eb8
Chiavetta Vincenzo <vincenzo_chiavetta@icloud.com>
2024-08-20 12:54:55 +0200

    fix: patched staging env

--- scripts/pre-build.sh
+++ scripts/pre-build.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-data="Sitemap: https://www$DEPLOY_ENV.dedoai.org/sitemap.xml"
-echo "$data" >> ./static/robots.txt
+
+echo "Sitemap: https://www$DEPLOY_ENV.dedoai.org/sitemap.xml" >> ./static/robots.txt
+
+echo "
+# Recaptcha
+[recaptcha]
+key = \"$RECAPTCHA_KEY\"" >> ./config/_default/params.toml
+
+echo "
+# Apis
+[apis]
+basePath = \"$API_BASE_PATH\"" >> ./config/_default/params.toml
